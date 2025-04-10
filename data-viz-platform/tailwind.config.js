023c470a27a460334f846adb28fccada202b4310
Kevin Brennan <kevinmbrennan08@gmail.com>
2025-04-10 14:07:46 -0700

    Initialize vite app

---
+++ data-viz-platform/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html", // Path to your HTML file
+      "./src/**/*.{js,ts,jsx,tsx}", // Path to your JavaScript/TypeScript files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
